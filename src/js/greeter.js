31a759bf980a32a194c6508204eea24e38bd257f
Kanhaiya Thakur <kanthaku1@publicisgroupe.net>
2018-07-24 17:10:36 +0530

    Adding changes

--- src/js/greeter.js
+++ src/js/greeter.js
@@ -1,1 +1,17 @@
-document.write('<h1>Hello</h1>');
+import {createHTMLElement} from './service';
+
+//console.log(createElement());
+document.querySelector('body').appendChild(createElement('Ocean'));
+
+// Creating element.
+function createElement(name)
+{
+    const element = createHTMLElement(`<h1><span>Hello ${name}<span></h1>`);
+    //console.log(element);
+    
+    element.querySelector('span').addEventListener('click', function() {
+        console.log("Hello");
+    })
+
+    return element;
+};
---
+++ src/js/service.js
@@ -0,0 +1,6 @@
+export function createHTMLElement(htmlString) 
+{
+    const template = document.createElement('template');
+    template.innerHTML = htmlString;
+    return template.content.firstElementChild;
+}
